defc4a1948c72c78e19a917d5707e76820f5210c
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-06-01 11:41:09 -0300

    :hammer: Refactor xml to json converting functions into their own module

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const path = require('path');
 const fs = require('fs-extra');
 const json2xls = require('json2xls');
 
-const getExcelAsJSON = require('./lib/getExcelAsJSON');
+const XmlToJSON = require('./lib/XmlToJSON');
 
 exports = module.exports = async function(path, output = 'output.xlsx', fn) {
-  var json = await getExcelAsJSON(path);
+  var json = await XmlToJSON.fromDir(path);
   var data = fn(json);
   var xls = json2xls(data);
   fs.writeFileSync(output, xls, 'binary');
