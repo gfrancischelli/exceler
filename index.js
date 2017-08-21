717a00b4edfc53b7b33eb388f041af6abe982529
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-08-21 14:00:40 -0300

    :bug: Check if the path is a directory

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const fs = require('fs-extra');
 const json2xls = require('json2xls');
+const isDir = require('is-directory');
 
 const XmlToJSON = require('./lib/XmlToJSON');
 
 exports = module.exports = async function(path, output = 'output.xlsx', fn) {
-  var json = await XmlToJSON.fromDir(path);
-  var data = fn(json);
-  var xls = json2xls(data);
+  const json = isDir.sync(path)
+    ? await XmlToJSON.fromDir(path)
+    : await XmlToJSON.fromFile(path);
+
+  const data = fn(json);
+  const xls = json2xls(data);
+
   fs.writeFileSync(output, xls, 'binary');
   return data;
 };
